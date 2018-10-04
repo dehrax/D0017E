565241651698a904f23124c0f7e80a495a14c6ad
Tobias Skarhed <tobias@skarhed.com>
2018-10-04 16:48:55 +0200

    Add boilerplate files Lab2

---
+++ labs/lab2/Makefile
@@ -0,0 +1,6 @@
+
+all:
+	gcc -Wall -o lab2 lab2.c lab2_funcs.c -lm
+
+clean:
+	rm -f lab2
---
+++ labs/lab2/lab2_funcs.h
@@ -0,0 +1,18 @@
+#ifndef __LAB2_FUNCS_H__
+#define __LAB2_FUNCS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include "lab2.h"
+
+
+// add all other functions below
+int clear(char var);
+int calc(char r, char x, char y, char op);
+
+
+#endif /* __LAB2_FUNCS_H__ */
+
