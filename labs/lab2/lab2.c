e18b2ead2f282762a9867e1264eeb065428a27d9
Tobias Skarhed <tobias@skarhed.com>
2018-10-12 16:34:27 +0200

    Add show and find_var #28

--- labs/lab2/lab2.c
+++ labs/lab2/lab2.c
@@ -4,14 +4,21 @@
 #include "lab2_funcs.h"
 #define MAX_BUFFER 255
 
+matlab_var_t vars[] = {
+    {'a', 0.0},
+    {'b', 0.0},
+    {'c', 0.0},
+    {'r', 0.0},
+    {'x', 0.0},
+    {'y', 0.0}
+};
+
+
+
 int processLine(const char *line){
     char input[MAX_BUFFER];
 
     strcpy(input, line);//We don't want that const
-    if(!strcmp(input, "quit")){
-        puts("\nOkay, bye!");
-        return 0;
-    }
 
     char* args[10];
     int argc = 0;
@@ -28,8 +35,12 @@ int processLine(const char *line){
         p++;
     }
 
-    if(!strcmp(args[0], "hello")){
-        puts("Hi on you too!");
+    if(!strcmp(args[0], "quit")){
+        puts("Okay, goodbye!");
+        return 0;
+    } else if(!strcmp(args[0], "show")){
+        //Takes first char in string
+        show(args[1][0]);
     }
 
     return 1;
--- labs/lab2/lab2_funcs.c
+++ labs/lab2/lab2_funcs.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
+#include "lab2.h"
 
-void printhelp(void); 
+void printhelp(void);
+
+matlab_var_t *find_var(char var){
+
+    //Loop through amount of variables and check the name
+    for(int i = 0; i<6;i++){
+        if(vars[i].n == var)
+            return &vars[i];
+    }
+    return NULL;
+}
+
+int show(char name){
+    matlab_var_t* var = find_var(name);
+    if(!var){
+        printf("Can't find variable called '%c'\n", name);
+    } else{
+        printf("%c = %g\n", name, var->v);
+        return 1;
+    }
+
+    return 0;
+}
