cc1f9cc4c04c325436919bdd6c523de92add85a4
Tobias Skarhed <tobias@skarhed.com>
2018-10-15 16:27:37 +0200

    Add basic mathematical operations and function for vector ops #29

---
+++ labs/lab2/operations.c
@@ -0,0 +1,24 @@
+#include "operations.h"
+
+double add(double a, double b){
+    return a + b;
+}
+
+double subtract(double a, double b){
+    return a - b;
+}
+
+int vectorOperation(matlab_arr_t *var, double (*func)(double), matlab_arr_t* result){
+    for(int i = 0; i<ARRAY_LEN; i++){
+        result->v[i] = func(var->v[i]);
+    }
+    return 0;
+}
+
+int vecOps(matlab_arr_t* var1, matlab_arr_t* var2, double (*func)(double, double), matlab_arr_t* result){
+    for(int i = 0; i < ARRAY_LEN; i++){
+        result->v[i] = func(var1->v[i], var2->v[i]);
+    }
+
+    return 0;
+}
